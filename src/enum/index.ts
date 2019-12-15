5569a777c3f81e77feefe1e22ce036868b6ed53a
RussellHowell <rhowell1110@gmail.com>
2019-12-15 17:54:21 -0500

    Added interfaces and enums related to RateQuote service query/response

---
+++ src/enum/index.ts
@@ -0,0 +1,20 @@
+/**
+ * @author Russell Howell
+ * @created Dec 15
+ *
+ * @description This file indexes enums used in the application 
+ * 
+ */
+
+export enum EPropertyType {
+     SINGLE_FAMILY = "SingleFamily",
+     CONDO = "Condo",
+     TOWNHOUSE = "Townhouse",
+     MULTI_FAMILY = "MultiFamily"
+ } 
+
+export enum EOccupancyType {
+    PRIMARY = "Primary",
+    SECONDARY = "Secondary",
+    INVESTMENT = "Investment"
+}
