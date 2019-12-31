f90a2d33ba92f050cffa6af997c813d17927d628
RussellHowell <rhowell1110@gmail.com>
2019-12-31 17:35:16 -0500

    <SimpleDataTable /> : Updated component function name, inital component & corresponding tests written

--- src/common/components/SimpleDataTable/SimpleDataTable.test.tsx
+++ src/common/components/SimpleDataTable/SimpleDataTable.test.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import TestRenderer from 'react-test-renderer';
 import SimpleDataTable from './SimpleDataTable';
 import { shallow, ShallowWrapper } from 'enzyme';
+import * as _ from 'lodash';
 
 
 const testColumns: Array<{ header: string, path: Array<string> | string }> = [
@@ -58,3 +59,23 @@ expect( thead ).toHaveLength( 1 );
 const thArr: ShallowWrapper = table.find( 'th' );
 //number of <th> tags should match number of test columns
 expect( thArr ).toHaveLength( testColumns.length ); 
+//content of <th> tags should match content of columns prop 
+thArr.map( ( th, index ) => {
+    expect( th.text() ).toMatch( testColumns[index].header );
+});
+
+const tbody: ShallowWrapper = table.find( 'tbody' );
+//table should render exactly 1 tbody 
+expect( tbody ).toHaveLength( 1 ); 
+
+const trArr: ShallowWrapper = tbody.find( 'tr' );
+//table should render the same number as rows as entries in the testRowData array
+expect( trArr ).toHaveLength( testRowData.length ); 
+//each row's content should match its correspending entry in testRowData 
+trArr.map( ( row, testRowIndex ) => {
+    const tdArr: ShallowWrapper = row.find( 'td' );
+    //make sure each row has the proper number of entries 
+    expect( tdArr ).toHaveLength( testColumns.length ); 
+    //make sure each cell contains the correct content
+    tdArr.map( ( cell, columnIndex ) => expect( cell.text() ).toMatch( _.get( testRowData[testRowIndex], testColumns[columnIndex].path ).toString() ) ) 
+} ) 
--- src/common/components/SimpleDataTable/SimpleDataTable.tsx
+++ src/common/components/SimpleDataTable/SimpleDataTable.tsx
@@ -1,11 +1,19 @@
+/**
+ * @author Russell Howell
+ * 
+ */
+
+
 import * as React from 'react';
+import * as _ from 'lodash';
 import { IBaseProps, IDataTableProps } from '../../interface';
+
 
 export interface ISimpleDataTableProps extends IDataTableProps, IBaseProps {
 }
 
 /** Table component that renders an HTML table based on a passed array of column models and row data */
-const DataTable: React.FC<ISimpleDataTableProps> = (props, context) => {
+const SimpleDataTable: React.FC<ISimpleDataTableProps> = (props, context) => {
     return (
       <div className={ props.className } style={ props.style }>
         <table>
@@ -14,9 +22,21 @@ const DataTable: React.FC<ISimpleDataTableProps> = (props, context) => {
                 { props.columns.map( ( columnModel, index ) => <th key={ 'col-'+index }>{ columnModel.header } </th> ) }
               </tr>
             </thead>
+            <tbody>
+                { props.rowData.map( ( row, rowIndex ) => ( 
+                <tr key={ 'row-'+rowIndex }> 
+                  {
+                    props.columns.map( ( columnModel, columnIndex ) => ( 
+                    <td key={ `${rowIndex}-${columnIndex}`}> 
+                      { _.get( row, columnModel.path ) }
+                    </td> ) )
+                  } 
+                </tr> 
+                ) ) }
+            </tbody>
         </table>
       </div>
     );
   };
 
-export default DataTable;
+export default SimpleDataTable;
