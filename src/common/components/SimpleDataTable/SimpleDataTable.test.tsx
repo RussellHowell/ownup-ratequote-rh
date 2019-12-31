7fa11b139804e8dc783a10df559f2954aa86452c
RussellHowell <rhowell1110@gmail.com>
2019-12-31 11:04:36 -0500

    Generated inital snapshot test for SimpleDataTable

--- src/common/components/SimpleDataTable/SimpleDataTable.test.tsx
+++ src/common/components/SimpleDataTable/SimpleDataTable.test.tsx
@@ -1,7 +1,12 @@
-const testColumns: Array<{ header: string, accessor: Array<string> | string }> = [
-    { header: 'Name', accessor: 'name' },
-    { header: 'Age', accessor: [ 'stats', '0' ] },
-    { header: 'Height', accessor: ['stats', '1' ] }
+import * as React from 'react';
+import TestRenderer from 'react-test-renderer';
+import SimpleDataTable from './SimpleDataTable';
+
+
+const testColumns: Array<{ header: string, path: Array<string> | string }> = [
+    { header: 'Name', path: 'name' },
+    { header: 'Age', path: [ 'stats', '0' ] },
+    { header: 'Height', path: ['stats', '1' ] }
 ]
 
 const testRowData: Array<{
@@ -21,3 +26,14 @@ const testRowData: Array<{
          stats: [ 46, "5'8\"" ]
      }
  ]
+
+ it('renders correctly', () => {
+    const tree = TestRenderer.create(
+    <SimpleDataTable
+        columns={ testColumns }
+        rowData={ testRowData }
+    />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+ 
