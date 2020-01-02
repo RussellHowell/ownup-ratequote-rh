abfcd9a65c23428acd4daca8f9827bf9a025541a
RussellHowell <rhowell1110@gmail.com>
2020-01-02 16:03:31 -0500

    Added missing file from previous commit

---
+++ src/redux/sagas/index.ts
@@ -0,0 +1,15 @@
+import { takeLatest } from 'redux-saga/effects';
+import { EActionTypes } from '../actions/actions';
+import { requestRateQuotes } from './requestRateQuotes.saga';
+
+/** 
+ * @author Russell Howell
+ * 
+ * @description entrypoint for sagas middleware
+ * 
+ */
+
+ /** Root saga that should be called to set up application at runtime */
+ export default function* rootSaga(){
+    takeLatest( EActionTypes.RATE_QUOTE_REQUEST, requestRateQuotes );
+ }      
