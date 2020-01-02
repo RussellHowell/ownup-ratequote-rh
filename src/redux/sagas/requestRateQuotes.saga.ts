e5a3785e80d4455dc88ca00941aa8ebf382646db
RussellHowell <rhowell1110@gmail.com>
2020-01-02 14:14:44 -0500

    Added redux sagas for rateQuoteRequest

---
+++ src/redux/sagas/requestRateQuotes.saga.ts
@@ -0,0 +1,15 @@
+import { IRateQuoteServiceResponse, IRateQuoteServiceQuery } from "../../rate-quote";
+import { EActionTypes, onRateQuoteRequestSuccess, onRateQuoteRequestFailure } from "../actions/actions";
+import { IReduxAction } from "../../common";
+import { call, put } from 'redux-saga/effects'
+import { fetchRateQuotes } from "../../rate-quote/service";
+
+export function* requestRateQuotes( action: IReduxAction<EActionTypes, { id: string, content: IRateQuoteServiceQuery}>  ){
+    try {
+        const response: IRateQuoteServiceResponse = yield call( fetchRateQuotes, action.payload.content );
+        yield put( onRateQuoteRequestSuccess( { id: action.payload.id, content: response } ) );
+    }catch( e ){
+        console.error( e ); 
+        yield put( onRateQuoteRequestFailure( { id: action.payload.id, errorMessages: e } ) )
+    }
+}
