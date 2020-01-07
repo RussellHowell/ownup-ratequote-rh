70b96e71ee1e57676733698d686c63216e32bba3
Russell Howell <rhowell1110@gmail.com>
2020-01-06 23:40:32 -0500

    Fixed imports in redux index file

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,7 +1,7 @@
+import rootReducer from './reducers';
+
 export * from './actions';
 export * from './reducers';
 export * from './sagas';
 
-
-import rootReducer from './reducers';
 export default rootReducer;
