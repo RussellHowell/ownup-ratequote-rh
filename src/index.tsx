d842dd2183689db27474a5fad6d71d23678102ee
Russell Howell <rhowell1110@gmail.com>
2020-01-07 14:37:35 -0500

    Set up Redux middleware and provider at application root

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducer  from './redux';
 import rootSaga from './redux/sagas';
 
+
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore( rootReducer, applyMiddleware( sagaMiddleware ) );
+//set up for redux devtools 
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore( rootReducer, composeEnhancers( applyMiddleware( sagaMiddleware ) ) );
+
 //start sagas action listener(s)
 sagaMiddleware.run( rootSaga ); 
 
